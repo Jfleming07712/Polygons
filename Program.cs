454bb4f6c41817b1c0c7c4bc4b6095a0843bd2a7
Jfleming <Jfleming07712@gmail.com>
2019-06-17 12:28:18 -0400

    Polygons program for interface tutorial.  Redo.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Polygons.Library;
+using System;
 
 namespace Polygons
 {
@@ -6,7 +7,35 @@ namespace Polygons
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Universe!");
+            var square = new Square(5);
+            DisplayPolygon("Square", square);
+
+            //var triangle = new Triangle(5);
+            //DisplayPolygon("Triangle", triangle);
+
+            //var octagon = new Octagon(5);
+            //DisplayPolygon("Octagon", octagon);
+
+            Console.Read();
+        }
+
+        public static void DisplayPolygon(string polygonType, dynamic polygon)
+        {
+            try
+            {
+                Console.WriteLine($"{polygonType} Number of Sides: {polygon.NumberOfSides}");
+                Console.WriteLine($"{polygonType} Side Length: {polygon.SideLength}");
+                Console.WriteLine($"{polygonType} Perimeter: {polygon.GetPerimeter()}");
+                Console.WriteLine($"{polygonType} Area: {Math.Round(polygon.GetArea(), 2)}");
+                Console.WriteLine();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(
+                    $"Exception was thrown while trying to process {polygonType}:\n   {ex.GetType().Name}");
+                Console.WriteLine();
+            }
         }
     }
 }
